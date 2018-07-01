225d8d7252aa8cfef5db39a5d59d51acdccc8fb2
admin <brad@brad.tw>
2018-07-01 11:34:22 +0800

    2018070106

--- src/brad02.js
+++ src/brad02.js
@@ -1,15 +1,49 @@
 
 var Brad02Layer = cc.Layer.extend({
+    drawing:null,
+    isDrawing: false,
     ctor:function () {
         this._super();
 
+        this.drawing = new cc.DrawNode();
+        this.addChild(this.drawing);
 
-
-
-
+        this.setupMouse();
 
         return true;
     },
+
+    setupMouse: function () {
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            lastX: -1,
+            lasyY: -1,
+            onMouseDown: function (e) {
+                var target = e.getCurrentTarget();
+                target.isDragging = true;
+                this.lastX = e.getLocationX();
+                this.lasyY = e.getLocationY();
+            },
+            onMouseUp: function (e) {
+                var target = e.getCurrentTarget();
+                target.isDragging = false;
+            },
+            onMouseMove: function (e) {
+                var target = e.getCurrentTarget();
+                if (!target.isDragging) return;
+                var x = e.getLocationX(), y = e.getLocationY();
+                target.drawing.drawSegment(
+                    cc.p(this.lastX,this.lasyY),
+                    cc.p(x,y),
+                    2, cc.color(255,0,0));
+                this.lastX = x; this.lasyY = y;
+            }
+
+
+        },this);
+
+
+    }
 
 
 });
